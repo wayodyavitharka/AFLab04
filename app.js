e62cde3f7dcf58c2522432d362423f1b02722485
vitharka <wayodyavitharka@gmail.com>
2026-02-23 14:07:25 +0530

    Use a working promise

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const myModule = require('./my-module.js');
-console.log(myModule.myFunction());
+const condition = true;
+
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve('Success!');
+  } else {
+    reject('Failure!');
+  }
+});
+
+myPromise.then((result) => {
+  console.log(result);
+}).catch((error) => {
+  console.log(error);
+});
 
