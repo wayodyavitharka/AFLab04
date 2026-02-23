8774ff3f781deabb64e0f59dafae382ae5c6b957
vitharka <wayodyavitharka@gmail.com>
2026-02-23 13:25:37 +0530

    Creating a web server

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-console.log("Lab started");
-
 const fs = require('fs');
 
-fs.readFile('file.txt', 'utf8', function (err, data) {
-if (err) throw err;
-console.log(data);
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+  if (err) throw err;
+  console.log('File saved!');
 });
+
