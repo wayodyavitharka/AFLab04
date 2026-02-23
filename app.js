80cadfa81561fe85ca9b37b9dac81622638b2e2f
vitharka <wayodyavitharka@gmail.com>
2026-02-23 14:10:35 +0530

    Use Async Await

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const condition = true;
 
 const myPromise = new Promise((resolve, reject) => {
-  if (condition) {
-    resolve('Success!');
-  } else {
-    reject('Failure!');
-  }
+  if (condition) resolve('Success!');
+  else reject('Failure!');
 });
 
-myPromise.then((result) => {
-  console.log(result);
-}).catch((error) => {
-  console.log(error);
-});
+async function myFunction() {
+  try {
+    const result = await myPromise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+myFunction();
 
